da74826452655884c6161e324957f32a70f8eb78
jillurquddus <jillur.quddus@hyperlearning.ai>
2022-05-25 15:07:20 +0100

    Use string arrays over StatefulBeanToCsvBuilder

--- pob-data/pob-publishers/src/main/java/ai/hyperlearning/pob/data/publishers/csv/CsvPublisher.java
+++ pob-data/pob-publishers/src/main/java/ai/hyperlearning/pob/data/publishers/csv/CsvPublisher.java
@@ -8,9 +8,8 @@ import org.slf4j.LoggerFactory;
 
 import com.opencsv.CSVWriter;
 import com.opencsv.ICSVWriter;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
 
+import ai.hyperlearning.pob.core.utils.FileUtils;
 import ai.hyperlearning.pob.data.publishers.OpportunityPublisher;
 import ai.hyperlearning.pob.data.publishers.exceptions.OpportunityPublishingException;
 import ai.hyperlearning.pob.model.Opportunity;
@@ -29,6 +28,10 @@ public class CsvPublisher extends OpportunityPublisher {
     
     // CSV properties
     private static final String CSV_FILE_PATH_PROPERTY_KEY = "path";
+    private static final String[] CSV_HEADER = {
+            "ID", "URI", "URL", "TITLE", "BUYER", 
+            "DATE_PUBLISHED", "DATE_CLOSING", "SUMMARY"
+    };
     
     public CsvPublisher(Map<String, Object> properties) {
         super(properties);
@@ -36,25 +39,39 @@ public class CsvPublisher extends OpportunityPublisher {
     
     public void publish(Opportunity opportunity) {
         
+        // Check whether the file exists before creating the OpenCSV writer
         LOGGER.info("Started the CSV publisher.");
         String filePath = (String) getProperties()
                 .get(CSV_FILE_PATH_PROPERTY_KEY);
-        try ( FileWriter fileWriter = new FileWriter(filePath, true) ) {
-            
-            // Build the OpenCSV Writer
-            CSVWriter csvWriter = new CSVWriter(fileWriter,
-                    ICSVWriter.DEFAULT_SEPARATOR,
-                    ICSVWriter.DEFAULT_QUOTE_CHARACTER,
-                    ICSVWriter.DEFAULT_ESCAPE_CHARACTER,
-                    ICSVWriter.DEFAULT_LINE_END);
-            
-            // Java objects to CSV builder
-            StatefulBeanToCsv<Opportunity> beanToCsv = 
-                    new StatefulBeanToCsvBuilder<Opportunity>(csvWriter)
-                        .build();
-            
-            // Append to the CSV file
-            beanToCsv.write(opportunity);
+        boolean fileExists = FileUtils.fileExists(filePath);
+        
+        try (
+                FileWriter fileWriter = new FileWriter(filePath, true);
+                CSVWriter csvWriter = new CSVWriter(fileWriter,
+                        ICSVWriter.DEFAULT_SEPARATOR,
+                        ICSVWriter.DEFAULT_QUOTE_CHARACTER,
+                        ICSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                        ICSVWriter.DEFAULT_LINE_END); ) {
+                
+                // Write the header record if required
+                if (!fileExists)
+                    csvWriter.writeNext(CSV_HEADER);
+                
+                // Convert the opportunity object into a String array.
+                // We will NOT use the StatefulBeanToCsvBuilder as this writes
+                // the header row before each append. See the following link:
+                // https://stackoverflow.com/questions/48922642/appending-to-csv-file-without-headers
+                csvWriter.writeNext(new String[]{
+                        opportunity.getId(), 
+                        opportunity.getUri(), 
+                        opportunity.getUrl(), 
+                        opportunity.getTitle(), 
+                        opportunity.getBuyer(), 
+                        opportunity.getDatePublished() != null ? 
+                                opportunity.getDatePublished().toString() : "", 
+                        opportunity.getDateClosing() != null ? 
+                                opportunity.getDateClosing().toString() : "", 
+                        opportunity.getSummary()});
             
         } catch (Exception e) {
             
